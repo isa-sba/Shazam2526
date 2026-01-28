48abc61d6c24895c700486d1f5bfd25eac877b6f
isa-sba <isabellasba1002@gmail.com>
2026-01-28 11:14:55 -0500

    odo auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -14,10 +14,17 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(9.752236);
+            .mass(9.752236)
+            .forwardZeroPowerAcceleration(-37.83701979673274)
+            .lateralZeroPowerAcceleration(-65.59593334966267)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.2, 0, 0.015, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(3, 0, 0.1, 0.01))
+            .centripetalScaling(0.005);
+
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
@@ -33,17 +40,22 @@ public class Constants {
             .rightFrontMotorName("fr")
             .rightRearMotorName("br")
             .leftRearMotorName("bl")
-            .leftFrontMotorName("lf")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+            .leftFrontMotorName("fl")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(57.7244739246117)
+            .yVelocity(43.127967841286456);
     public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
             .forwardEncoder_HardwareMapName("lo")
             .strafeEncoder_HardwareMapName("i")
             .IMU_HardwareMapName("imu")
             .strafePodX(-8.25)
             .forwardPodY(-3.25)
+            .strafeEncoderDirection(Encoder.REVERSE)
+            .forwardTicksToInches(0.002968434)
+            .strafeTicksToInches(0.002968434)
             .IMU_Orientation(
                     new RevHubOrientationOnRobot(
                             RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
